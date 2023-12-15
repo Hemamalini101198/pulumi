82f464914af5effd57853378b586d26de12bb134
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-12-15 22:49:28 +0530

    Update rds.py

--- rds.py
+++ rds.py
@@ -8,12 +8,39 @@ instance_class = config.require('instance_class')
 username = config.require('username')
 password = config.require('password')
 allocated_storage = config.require('allocated_storage')
+vpc_id = config.require('vpc_id')
 max_allocated_storage = config.require('max_allocated_storage')
-vpc_security_group_ids = config.require('vpc_security_group_ids')
 final_snapshot_identifier = config.require('final_snapshot_identifier')
+subnet_ids = config.require('subnet_ids')
 
-# Create RDS instance
-rds_db = aws.rds.Instance("rds_db",
+# Create a new security group
+rds_security_group = aws.ec2.SecurityGroup("rds-security-group",
+    description="Security group for RDS instance",
+    vpc_id=vpc_id,
+    egress=[{
+        "description": "Allow all outbound traffic",
+        "from_port": 0,
+        "to_port": 0,
+        "protocol": "-1",
+        "cidr_blocks": ["0.0.0.0/0"],
+    }],
+    ingress=[{
+        "description": "Allow MySQL",
+        "from_port": 5432,
+        "to_port": 5432,
+        "protocol": "tcp",
+        "cidr_blocks": ["0.0.0.0/0"],
+    }],
+)
+
+
+# Create an RDS Subnet Group
+rds_subnet_group = aws.rds.SubnetGroup("rds-subnet-group",
+    subnet_ids=["subnet_ids"],  
+)
+
+# Create RDS instance with the new security group and subnet group
+rds_instance = aws.rds.Instance("rds-instance",
     identifier=db_name,
     engine=engine_type,
     instance_class=instance_class,
@@ -25,15 +52,16 @@ rds_db = aws.rds.Instance("rds_db",
     publicly_accessible=True,
     multi_az=False,
     storage_encrypted=True,
-    vpc_security_group_ids=["vpc_security_group_ids"],
+    vpc_security_group_ids=[rds_security_group.id],
     performance_insights_enabled=True,
     performance_insights_retention_period=7,
     final_snapshot_identifier=final_snapshot_identifier,
     skip_final_snapshot=False,
     copy_tags_to_snapshot=True,
     deletion_protection=True,
+    db_subnet_group_name=rds_subnet_group.name,
 )
 
 # Export RDS instance properties
-pulumi.export("rds_db_id", rds_db.id)
-pulumi.export("rds_db_endpoint", rds_db.endpoint)
+pulumi.export("rds_instance_id", rds_instance.id)
+pulumi.export("rds_instance_endpoint", rds_instance.endpoint)
