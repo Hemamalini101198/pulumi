9c949372f5e1d7ac11fc1dbf12d839ef98488513
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-12-16 17:01:34 +0530

    Update ec2-instance.py

--- ec2-instance.py
+++ ec2-instance.py
@@ -7,7 +7,8 @@ key_name = config.require('key_name')
 instance_type = config.require('instance_type')
 instance_ami = config.require('instance_ami')
 instance_name = config.require('instance_name')
-vpc_id = config.require('vpc_id')  
+vpc_id = config.require('vpc_id')
+vpc_security_group_ids = config.require('security_group')
 subnet_id =  config.require('subnet_id')
 volume_size = config.require('volume_size')
 security_group_name = config.require('security_group_name')
@@ -34,43 +35,64 @@ private_key_object = aws.s3.BucketObject("privateKeyObject",
 )
 
 # Create a instance
-security_group = aws.ec2.SecurityGroup(
-    security_group_name,
-    vpc_id=vpc_id,
-    ingress=[
-        aws.ec2.SecurityGroupIngressArgs(
-            protocol="tcp",
-            from_port=22,
-            to_port=22,
-            cidr_blocks=["0.0.0.0/0"],
-        ),
-        aws.ec2.SecurityGroupIngressArgs(
-            protocol="tcp",
-            from_port=443,
-            to_port=443,
-            cidr_blocks=["0.0.0.0/0"],
-        ),
-    ],
-)
+if vpc_security_group_ids:
+    server = aws.ec2.Instance(
+        instance_name,
+        key_name=aws_key.key_name,
+        instance_type=instance_type,
+        ami=instance_ami,
+        subnet_id=subnet_id,
+        associate_public_ip_address=True,
+        root_block_device={
+        "volume_size": volume_size,
+        "volume_type": "gp2"
+        },
+        tags={
+            "Name": instance_name,
+        },
+        vpc_security_group_ids=[vpc_security_group_ids],
+    )
 
-server = aws.ec2.Instance(
-    instance_name,
-    key_name=aws_key.key_name,
-    instance_type=instance_type,
-    ami=instance_ami,
-    subnet_id=subnet_id,
-    associate_public_ip_address=True,
-    root_block_device={
-    "volume_size": volume_size, 
-    "volume_type": "gp2"
-    },
-    tags={
-        "Name": instance_name,
-    },
-    vpc_security_group_ids=[security_group.id],
-)
+else:
+    security_group = aws.ec2.SecurityGroup(
+        security_group_name,
+        vpc_id=vpc_id,
+        ingress=[
+            aws.ec2.SecurityGroupIngressArgs(
+                protocol="tcp",
+                from_port=22,
+                to_port=22,
+                cidr_blocks=["0.0.0.0/0"],
+            ),
+            aws.ec2.SecurityGroupIngressArgs(
+                protocol="tcp",
+                from_port=443,
+                to_port=443,
+                cidr_blocks=["0.0.0.0/0"],
+            ),
+        ],
+    )
+
+    server = aws.ec2.Instance(
+        instance_name,
+        key_name=aws_key.key_name,
+        instance_type=instance_type,
+        ami=instance_ami,
+        subnet_id=subnet_id,
+        associate_public_ip_address=True,
+        root_block_device={
+        "volume_size": volume_size,
+        "volume_type": "gp2"
+        },
+        tags={
+            "Name": instance_name,
+        },
+        vpc_security_group_ids=security_group.id,
+    )
+
 
 pulumi.export('private_key_pem', ssh_key.private_key_pem)
 pulumi.export('public_ip', server.public_ip)
 pulumi.export('public_dns', server.public_dns)
+
 
